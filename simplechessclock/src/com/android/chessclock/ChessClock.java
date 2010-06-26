360b935c18a761b7501f65601f4eed0a37f17954
eldarerathis <eldarerathis@gmail.com>
2010-06-26 18:59:29 -0400

    Fix for bug #1 (Rendering dies on screen rotate).

--- simplechessclock/src/com/android/chessclock/ChessClock.java
+++ simplechessclock/src/com/android/chessclock/ChessClock.java
@@ -1,4 +1,4 @@
-/***************************************************
+/*************************************************************************
  * File: Main.java
  * 
  * Implements the main form/class for Chess Clock.
@@ -7,7 +7,24 @@
  * 
  * Author: Carter Dewey
  * 
- ***************************************************/
+ *************************************************************************
+ *
+ *    This file is part of Simple Chess Clock (SCC).
+ *    
+ *   SCC is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SCC is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SCC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************************************************************************/
 
 package com.android.chessclock;
 
