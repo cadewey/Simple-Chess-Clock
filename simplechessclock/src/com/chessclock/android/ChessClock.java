0040a829055f90bd49554988ad18969c54322abb
eldarerathis <eldarerathis@gmail.com>
2010-11-29 21:30:15 -0500

    *SCC can be moved to SD storage now

--- simplechessclock/src/com/chessclock/android/ChessClock.java
+++ simplechessclock/src/com/chessclock/android/ChessClock.java
@@ -64,8 +64,8 @@ public class ChessClock extends Activity {
 	/** Version info and debug tag constants */
 	public static final String TAG = "INFO";
 	public static final String V_MAJOR = "1";
-	public static final String V_MINOR = "1";
-	public static final String V_MINI = "3";
+	public static final String V_MINOR = "2";
+	public static final String V_MINI = "0";
 
 	/** Constants for the dialog windows */
 	private static final int SETTINGS = 0;
